e24c33b877957d253f3b563832f22d959c40b171
Kgothatso Phooko <kgothatso.phooko@thegocompany.io>
2026-06-23 23:50:34 +0200

    Added base html functionality

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from articles.models import Article
 
-# Create your views here.
+
+def home(request):
+    articles = Article.objects.filter(status="published")
+
+    context = {
+        "latest_articles": articles.order_by("-created_at"),  # main content
+        "oldest_articles": articles.order_by("created_at"),   # sidebar
+    }
+
+    return render(request, "articles/list.html", context)
